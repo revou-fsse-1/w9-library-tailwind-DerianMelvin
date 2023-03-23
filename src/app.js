750fda9c77fe13b6b0f03fc50f0b5c2c4b05de51
Derian Melvin <derianmelvin88@gmail.com>
2023-03-23 15:43:43 +0700

    Add tailwind styling to navbar & footer - Add button interaction

--- src/app.js
+++ src/app.js
@@ -1,22 +1,37 @@
+function toggleMenuButton() {
+  const menu = document.getElementById("m-menu");
+  const buttonChildren = document.getElementById("m-menu-button").children;
+
+  if (menu.classList.contains("hidden")) {
+    menu.classList.replace("hidden", "block");
+    buttonChildren[1].classList.replace("block", "hidden");
+    buttonChildren[2].classList.replace("hidden", "block");
+  } else {
+    menu.classList.replace("block", "hidden");
+    buttonChildren[1].classList.replace("hidden", "block");
+    buttonChildren[2].classList.replace("block", "hidden");
+  }
+}
+
 function getPageFromUrl() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
 
-    let page = urlParams.get('page');
+  let page = urlParams.get("page");
 
-    return page;
+  return page;
 }
 
 async function getBooksData() {
-    let response = await fetch('./data.json');
-    let json = await response.json();
-    return json['books'];
+  let response = await fetch("./data.json");
+  let json = await response.json();
+  return json["books"];
 }
 
 /**
  * To get books data, you should make sure that your function is async function
  * and call `getBooksData` function with await.
- * 
+ *
  * e.g.
  * async function yourFunction() {
  *      let books = await getBooksData();
